70d415503c5b2e49462a043ce670ec4ca27bf810
peaugust <peaugust@hotmail.com>
2017-11-10 20:41:03 -0200

    Implementando enum Acontecimento Registro

--- SistemaControleAcesso2/src/br/ufsc/ine5605/sistemacontroleacesso2/AcontecimentoRegistro.java
+++ SistemaControleAcesso2/src/br/ufsc/ine5605/sistemacontroleacesso2/AcontecimentoRegistro.java
@@ -9,6 +9,41 @@ package br.ufsc.ine5605.sistemacontroleacesso2;
  *
  * @author PEaug
  */
-class AcontecimentoRegistro {
+public enum AcontecimentoRegistro {
+      //Enums:
+    /**Enum que representa a tentativa de acesso a porta do
+     * financeiro fora do horário permitido pelo cargo
+     */
+    FORADEHORARIO("O Usuario em questao tem acesso a porta, porem tentou"
+                  + " acessa-la fora do horario permitido pelo seu cargo."),
     
+    /**Enum que representa a tentativa de acesso a porta do financeiro por um
+     * cargo que não tem acesso 
+     */
+    CARGOSEMACESSO("O Usuario em questao nao possui acesso a porta e"
+                   + " tentou abri-la.");
+    
+    //Atributos
+    /**
+     * Atributo do Enum que representa a descrição do registro
+     */
+    private String descricao;
+    
+    //Construtor
+    /**Construtor da descrição do Enum
+     * 
+     * @param descricao - Descrição do Acontecimento a ser registrado
+     */
+    AcontecimentoRegistro (String descricao) {
+        this.descricao = descricao;
+    }
+    
+    //Metodos
+    /**
+     * Método para retorno da descrição de um Enum
+     * @return String - Descrição do registro
+     */
+    public String getDescricao() {
+        return descricao;
+    }
 }
