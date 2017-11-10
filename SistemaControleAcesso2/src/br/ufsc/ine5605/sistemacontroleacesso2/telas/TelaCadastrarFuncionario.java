913263d338469b2b6de3585ca92aaa359ccd2329
joaovenancio <joaovitorvenancio99@outlook.com>
2017-11-10 14:33:03 -0200

    Adicionado os campos de texto e os rotulos

--- SistemaControleAcesso2/src/br/ufsc/ine5605/sistemacontroleacesso2/telas/TelaCadastrarFuncionario.java
+++ SistemaControleAcesso2/src/br/ufsc/ine5605/sistemacontroleacesso2/telas/TelaCadastrarFuncionario.java
@@ -11,6 +11,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
 /**
@@ -20,8 +21,22 @@ import javax.swing.WindowConstants;
 public class TelaCadastrarFuncionario extends JFrame {
     //Atributos:
     private JLabel labelMatricula;
-    
-    
+    private JTextField campoMatricula;
+    private JLabel labelNome;
+    private JTextField campoNome;
+    private JLabel labelDia;
+    private JTextField campoDia;
+    private JLabel labelMes;
+    private JTextField campoMes;
+    private JLabel labelAno;
+    private JTextField campoAno;
+    private JLabel labelTelefone;
+    private JTextField campoTelefone;
+    private JLabel labelSalario;
+    private JTextField campoSalario;
+    private JLabel labelCargo;
+    private JTextField campoCargo;
+            
     //Construtor:
     public TelaCadastrarFuncionario () {
         //Metodo para configurar a GUI:
@@ -51,16 +66,202 @@ public class TelaCadastrarFuncionario extends JFrame {
         GridBagConstraints constraints = new GridBagConstraints();
         
         //Definir o primeiro rotulo:
-        this.labelMatricula = new JLabel("Insira um numero de matricula:");
+        this.labelMatricula = new JLabel("Insira um numero de matricula:     ");
         //Definir suas especificacoes dentro do GridBagLayout:
         constraints.gridx = 0;
         constraints.gridy = 0;
-        constraints.weightx = 2;
-        constraints.weighty = 1;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
         constraints.anchor = GridBagConstraints.LINE_END;
         constraints.fill = GridBagConstraints.BOTH;
-        //Adicionar esse rotula ao meu container:
+        //Adicionar esse rotulo ao meu container:
         painel.add(this.labelMatricula, constraints);
+        
+        //Definir o rotulo do nome:
+        this.labelNome = new JLabel("Nome do funcionario:     ");
+        //Definir suas especificacoes dentro do GridBagLayout:
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.anchor = GridBagConstraints.LINE_END;
+        constraints.fill = GridBagConstraints.BOTH;
+        //Adicionar esse rotulo ao meu container:
+        painel.add(this.labelNome, constraints);
+        
+        //Definir o Rotulo do dia do nascimento:
+        this.labelDia = new JLabel("Dia do nascimento:     ");
+        //Definir suas especificacoes dentro do GridBagLayout:
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.anchor = GridBagConstraints.LINE_END;
+        constraints.fill = GridBagConstraints.BOTH;
+        //Adicionar esse rotulo ao meu container:
+        painel.add(this.labelDia, constraints);
+        
+        //Definir o  rotulo do mes:
+        this.labelMes = new JLabel("Mes do nascimento:     ");
+        //Definir suas especificacoes dentro do GridBagLayout:
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.anchor = GridBagConstraints.LINE_END;
+        constraints.fill = GridBagConstraints.BOTH;
+        //Adicionar esse rotulo ao meu container:
+        painel.add(this.labelMes, constraints);
+        
+        //Definir o rotulo do ano:
+        this.labelAno = new JLabel("Ano do nascimento:     ");
+        //Definir suas especificacoes dentro do GridBagLayout:
+        constraints.gridx = 0;
+        constraints.gridy = 4;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.anchor = GridBagConstraints.LINE_END;
+        constraints.fill = GridBagConstraints.BOTH;
+        //Adicionar esse rotulo ao meu container:
+        painel.add(this.labelAno, constraints);
+        
+        //Definir o rotulo do telefone:
+        this.labelTelefone = new JLabel("Telefone do funcionario:     ");
+        //Definir suas especificacoes dentro do GridBagLayout:
+        constraints.gridx = 0;
+        constraints.gridy = 5;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.anchor = GridBagConstraints.LINE_END;
+        constraints.fill = GridBagConstraints.BOTH;
+        //Adicionar esse rotulo ao meu container:
+        painel.add(this.labelTelefone, constraints);
+        
+        //Definir o rotulo do salario:
+        this.labelSalario = new JLabel("Salario:     ");
+        //Definir suas especificacoes dentro do GridBagLayout:
+        constraints.gridx = 0;
+        constraints.gridy = 6;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.anchor = GridBagConstraints.LINE_END;
+        constraints.fill = GridBagConstraints.BOTH;
+        //Adicionar esse rotulo ao meu container:
+        painel.add(this.labelSalario, constraints);
+        
+        //############ Colocar botao para ver todos os cargos (listar eles) ##############
+        
+        //Definir o rotulo do cargo:
+        this.labelCargo = new JLabel("Digite o código do cargo:     ");
+        //Definir suas especificacoes dentro do GridBagLayout:
+        constraints.gridx = 0;
+        constraints.gridy = 7;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.anchor = GridBagConstraints.LINE_END;
+        constraints.fill = GridBagConstraints.BOTH;
+        //Adicionar esse rotulo ao meu container:
+        painel.add(this.labelCargo, constraints);
+        
+        //
+        //Campos:
+        //
+        
+        //Colocar os campos para inserir dados:
+        this.campoMatricula = new JTextField(15);
+        //Definir as suas configurações:
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.anchor = GridBagConstraints.LINE_START;
+        constraints.fill = GridBagConstraints.BOTH;
+        //Adicionar o campo ao meu container:
+        painel.add(this.campoMatricula, constraints);
+        
+        //Colocar os campos para inserir dados:
+        this.campoNome = new JTextField(15);
+        //Definir as suas configurações:
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.anchor = GridBagConstraints.LINE_START;
+        constraints.fill = GridBagConstraints.BOTH;
+        //Adicionar o campo ao meu container:
+        painel.add(this.campoNome, constraints);
+        
+        //Colocar os campos para inserir dados:
+        this.campoDia = new JTextField(15);
+        //Definir as suas configurações:
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.anchor = GridBagConstraints.LINE_START;
+        constraints.fill = GridBagConstraints.BOTH;
+        //Adicionar o campo ao meu container:
+        painel.add(this.campoDia, constraints);
+        
+        //Colocar os campos para inserir dados:
+        this.campoMes = new JTextField(15);
+        //Definir as suas configurações:
+        constraints.gridx = 1;
+        constraints.gridy = 3;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.anchor = GridBagConstraints.LINE_START;
+        constraints.fill = GridBagConstraints.BOTH;
+        //Adicionar o campo ao meu container:
+        painel.add(this.campoMes, constraints);
+        
+        //Colocar os campos para inserir dados:
+        this.campoAno = new JTextField(15);
+        //Definir as suas configurações:
+        constraints.gridx = 1;
+        constraints.gridy = 4;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.anchor = GridBagConstraints.LINE_START;
+        constraints.fill = GridBagConstraints.BOTH;
+        //Adicionar o campo ao meu container:
+        painel.add(this.campoAno, constraints);
+        
+        //Colocar os campos para inserir dados:
+        this.campoTelefone = new JTextField(15);
+        //Definir as suas configurações:
+        constraints.gridx = 1;
+        constraints.gridy = 5;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.anchor = GridBagConstraints.LINE_START;
+        constraints.fill = GridBagConstraints.BOTH;
+        //Adicionar o campo ao meu container:
+        painel.add(this.campoTelefone, constraints);
+        
+        //Colocar os campos para inserir dados:
+        this.campoSalario = new JTextField(15);
+        //Definir as suas configurações:
+        constraints.gridx = 1;
+        constraints.gridy = 6;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.anchor = GridBagConstraints.LINE_START;
+        constraints.fill = GridBagConstraints.BOTH;
+        //Adicionar o campo ao meu container:
+        painel.add(this.campoSalario, constraints);
+        
+        //Colocar os campos para inserir dados:
+        this.campoCargo = new JTextField(15);
+        //Definir as suas configurações:
+        constraints.gridx = 1;
+        constraints.gridy = 7;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.anchor = GridBagConstraints.LINE_START;
+        constraints.fill = GridBagConstraints.BOTH;
+        //Adicionar o campo ao meu container:
+        painel.add(this.campoCargo, constraints);
         
     }
     
