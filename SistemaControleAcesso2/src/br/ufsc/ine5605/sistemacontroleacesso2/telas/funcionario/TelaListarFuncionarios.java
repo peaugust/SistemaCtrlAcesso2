99a922fc1024031d9355c31b234b4e9125b941e2
joaovenancio <joaovitorvenancio99@outlook.com>
2017-11-13 21:52:21 -0200

    TelaListarFuncionarios iniciada

---
+++ SistemaControleAcesso2/src/br/ufsc/ine5605/sistemacontroleacesso2/telas/funcionario/TelaListarFuncionarios.java
@@ -0,0 +1,19 @@
+
+package br.ufsc.ine5605.sistemacontroleacesso2.telas.funcionario;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Joao Vitor Venancio
+ */
+public class TelaListarFuncionarios extends JFrame {
+    //Atributos:
+    
+    //Construtor:
+    public TelaListarFuncionarios () {
+        
+    }
+    //Metodos:
+    
+}
