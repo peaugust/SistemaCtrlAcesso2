0f6d8d92263f3cdd9628ef2940bf295fce79f4a2
peaugust <peaugust@hotmail.com>
2017-11-20 04:13:39 -0200

    Criada a primeira classe que estende exceção

---
+++ SistemaControleAcesso2/src/br/ufsc/ine5605/sistemacontroleacesso2/execoes/MatriculaInexistente.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufsc.ine5605.sistemacontroleacesso2.execoes;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author PEaug
+ */
+public class MatriculaInexistente extends IllegalArgumentException {
+
+    public MatriculaInexistente() {
+        super("Não existe funcionário com essa matrícula");
+    }
+    
+    
+}
